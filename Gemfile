89b9415b323771e666646a5c0849416c6cacb343
brianthecoder <brian@20jeans.com>
2013-10-03 14:28:33 -0700

    isolated spree bug test

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+gem 'rails', '4.0.0'
+gem 'sqlite3'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+gem 'spree_api', github: 'spree/spree', branch: '2-1-stable'
+gem 'spree_backend', github: 'spree/spree', branch: '2-1-stable'
+gem 'spree_cmd', github: 'spree/spree', branch: '2-1-stable'
+gem 'spree_core', github: 'spree/spree', branch: '2-1-stable'
+gem 'spree_gateway', github: 'spree/spree_gateway', branch: '2-1-stable'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpreeTest::Application.config.secret_key_base = 'e018ee9c8eee0ef3bab79bcb3ead41150d019b9bb3ebade96a0f5b284daead244a783f6846996130e9a5ceeba89196d446ffc0bf8ee503f037fc7f175aef1c6b'
